c37588da4c34fc447579a041a87bd835b01ec4de
Meowveloper <meowveloper.dev@gmail.com>
2025-05-17 02:38:00 +0700

    connection

--- src/configs/socket.onconnection.ts
+++ src/configs/socket.onconnection.ts
@@ -1,9 +1,10 @@
 import {DefaultEventsMap, Socket} from "socket.io";
 import Auth_Service from "@src/modules/auth/auth.service";
 
-export default async function on_connection(socket : Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
+export default async function on_connection(socket : Socket<DefaultEventsMap, DefaultEventsMap>) {
     const user_id = socket.handshake.auth.user_id as number;
     if(!user_id) { socket.disconnect(); return; }
+    socket.data.user_id = user_id;
     await Auth_Service.update_user(user_id, { active_now : true });
 
     console.log('user connected', user_id);
